7fc90daae242d82b55d2348df4d1201291b80b55
AnirudhW10Desk <anirudhmv21@gmail.com>
2019-12-09 23:18:38 +0530

    updated views.py
    updated views.py and removed .swp files and also removed .save files

--- main_website/views.py
+++ main_website/views.py
@@ -32,7 +32,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin,CreateView):
 	model=Post
-	fields=['title','content']
+	fields=['title','content','con_image']
 	#form_class=PostForm
 	def form_valid(self, form):
 		form.instance.author=self.request.user
